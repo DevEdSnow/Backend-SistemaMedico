ea36e10afff97681c4fe36934677d09099eec48b
Isai Reyes <anonymus1201a@gmail.com>
2026-07-27 17:49:47 -0600

    Creacion del Mapeo del Usuario

--- src/main/java/com/example/sismedico/mapper/UsuarioMapper.java
+++ src/main/java/com/example/sismedico/mapper/UsuarioMapper.java
@@ -1,5 +1,51 @@
 package com.example.sismedico.mapper;
 
+import com.example.sismedico.dto.response.UsuarioResponse;
+import com.example.sismedico.entity.Usuario;
+
 public class UsuarioMapper {
-    
+
+    private UsuarioMapper() {
+        // Evita instancias
+    }
+
+    public static UsuarioResponse toResponse(Usuario usuario) {
+
+        if (usuario == null) {
+            return null;
+        }
+
+        return UsuarioResponse.builder()
+                .id(usuario.getId())
+                .uuid(usuario.getUuid())
+                .nombre(usuario.getNombre())
+                .apellido(usuario.getApellido())
+                .correo(usuario.getCorreo())
+                .telefono(usuario.getTelefono())
+                .direccion(usuario.getDireccion())
+                .fotoPerfil(usuario.getFotoPerfil())
+                .genero(usuario.getGenero())
+                .fechaNacimiento(usuario.getFechaNacimiento())
+
+                .rolId(
+                        usuario.getRol() != null
+                                ? usuario.getRol().getId()
+                                : null
+                )
+
+                .rol(
+                        usuario.getRol() != null
+                                ? usuario.getRol().getNombre().name()
+                                : null
+                )
+
+                .activo(usuario.getActivo())
+                .emailVerificado(usuario.getEmailVerificado())
+                .tokenFirebase(usuario.getTokenFirebase())
+                .ultimoAcceso(usuario.getUltimoAcceso())
+                .fechaRegistro(usuario.getFechaRegistro())
+                .ultimaActualizacion(usuario.getUltimaActualizacion())
+                .build();
+    }
+
 }
